0f7bc217c99ac41ed12f952c5e1f7736bfad0a0c
Kristoffer Walker <kixxauth@gmail.com>
2009-10-21 07:43:11 -0400

    improve: moved modules.js loader into packs/
    Now modules.js can be reached from a toolpack by using <script
    src="../modules.js" /> instead of using the full chrome URL.  This
    improvement makes cross platform compatability easier.
    
     Changes to be committed:
    	renamed:    ../modules.js -> modules.js

--- tree/packs/modules.js
+++ tree/packs/modules.js
@@ -1,7 +1,6 @@
 (function (GlobalObject)
 {
   // todo: this functionality should be moved into a firefox specific module.
-  // the memcache module should remain platform agnostic
   var shell = Components.classes["@mozilla.org/appshell/appShellService;1"]
             .getService(Components.interfaces.nsIAppShellService);
   var parentElement = shell.hiddenDOMWindow.document.documentElement;
