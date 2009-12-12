8ab0db05b107ad4307d4aa793b5ee0223495d859
Kristoffer Walker <kixxauth@gmail.com>
2009-12-12 10:45:14 -0500

    refactored platform utils api

--- tree/packs/platform/utils_1.js
+++ tree/packs/platform/utils_1.js
@@ -1,10 +1,4 @@
 // todo: #notCrossPlatform
 // We need to determine what platform we are on before
 // requireing the platform utils object.
-var utils = require("platform/firefox/utils_1"),
-    n;
-for (n in utils) {
-  if (utils.hasOwnProperty(n)) {
-    exports[n] = utils[n];
-  }
-}
+require("platform/bind").bind(require("platform/firefox/utils_1"), exports);
