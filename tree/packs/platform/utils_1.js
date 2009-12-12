c593ee28251479770f5a55ae17efc36be8ada027
Kristoffer Walker <kixxauth@gmail.com>
2009-12-12 10:12:29 -0500

    refactored platform/utils

--- tree/packs/platform/utils_1.js
+++ tree/packs/platform/utils_1.js
@@ -1,10 +1,10 @@
-// todo: sniff out the platform application and load the corresponding utils
-// package
-var platform = require("platform/firefox/utils_1");
-
-exports.getCurrentWindow = platform.getCurrentWindow;
-exports.getAllWindows = platform.getAllWindows;
-exports.installToolbarButton = platform.installToolbarButton;
-exports.file = platform.file;
-exports.openNewTab = platform.tabs.openNewTab;
-exports.console = platform.console;
+// todo: #notCrossPlatform
+// We need to determine what platform we are on before
+// requireing the platform utils object.
+var utils = require("platform/firefox/utils_1"),
+    n;
+for (n in utils) {
+  if (utils.hasOwnProperty(n)) {
+    exports[n] = utils[n];
+  }
+}
