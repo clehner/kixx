d261749a6e2eb20a361766508a541e4745181072
Kristoffer Walker <kixxauth@gmail.com>
2009-12-18 04:59:14 -0500

    updated platform/init.js for new annodb api

--- tree/packs/platform/init.js
+++ tree/packs/platform/init.js
@@ -16,79 +16,74 @@ function main() {
       // (global constants module or something)
       buttonId = "kixx-launcher-toolstrip",
       
-      chromeWin;
+      chromeWin,
+
+      toolbarId = "nav-bar", // id of the firefox toolbar
+
+      // id of the button our toolbar button will follow (the home button) 
+      afterId = "home-button",
       
-      chromeWin = utils.getMostRecentChromeWindow();
+      tb, // toolbar button xul element
 
-      function addToolbarButtonListener() {
-        // todo: using this method of event registration, if the user removed the
-        // toolbar button, and then replaces it, the event will have never been
-        // registered.
-        try {
-          // add the event listener
-          chromeWin.document.getElementById(buttonId).
-            addEventListener("command",
-            function onToolbarButtonCommand(e) {
-              require("platform/tabs_1").create(
-                {url: "chrome://kixx/content/packs/platform/panel/panel.html"});
-            }, true);
-        } catch(e) {
-          // if the user has removed the button, it will not be there.
-          Components.utils.reportError(e);
-          log.warn("installToolbarButton() skipped add listener. "+
-            "(the toolbar button may have been removed)");
-        }
-        log.info("Kixx platform loaded.");
-      }
+      // the set of new buttons
+      newButtonSet;
+      
+  chromeWin = utils.getMostRecentChromeWindow();
 
-      // check to see if we have installed the button before
-      annodb.get("toolbar-button-installed", utils.MOZID,
-      function installToolbarButton(success, result) {
-        var toolbarId = "nav-bar", // id of the firefox toolbar
+  function addToolbarButtonListener() {
+    // todo: using this method of event registration, if the user removed the
+    // toolbar button, and then replaces it, the event will have never been
+    // registered.
+    try {
+      // add the event listener
+      chromeWin.document.getElementById(buttonId).
+        addEventListener("command",
+        function onToolbarButtonCommand(e) {
+          require("platform/tabs_1").create(
+            {url: "chrome://kixx/content/packs/platform/panel/panel.html"});
+        }, true);
+    } catch(e) {
+      // if the user has removed the button, it will not be there.
+      Components.utils.reportError(e);
+      log.warn("installToolbarButton() skipped add listener. "+
+        "(the toolbar button may have been removed)");
+    }
+    log.info("Kixx platform loaded.");
+  }
 
-            // id of the button our toolbar button will follow (the home button) 
-            afterId = "home-button",
-            
-            tb = chromeWin.document.getElementById(toolbarId),
+  // if this script has run before, we don't want to re-install
+  // a toolbar button that the user removed
+  if (annodb.get("toolbar-button-installed", utils.MOZID)) {
+    addToolbarButtonListener();
+    return;
+  }
 
-            // the set of new buttons
-            newButtonSet;
+  tb = chromeWin.document.getElementById(toolbarId);
 
-        // if this script has run before, we don't want to re-install
-        // a toolbar button that the user removed
-        if(!(success ^ result)) {
-          addToolbarButtonListener();
-          return;
-        }
+  // if the toolbar button is already there,
+  // we don't want to re-install it.
+  if(tb.currentSet.indexOf(buttonId) !== -1) {
+    addToolbarButtonListener();
+    return;
+  }
 
-        dump(tb.currentSet.indexOf(buttonId) +"\n");
-        // if the toolbar button is already there,
-        // we don't want to re-install it.
-        if(tb.currentSet.indexOf(buttonId) != -1) {
-          addToolbarButtonListener();
-          return;
-        }
+  newButtonSet = ((currentSet.indexOf("urlbar-container") !== -1) ?
+      // Place the button before the urlbar
+      tb.currentSet.replace(/urlbar-container/, buttonId +",urlbar-container") :
+      // Place the button at the end of the urlbar
+      (tb.currentSet + buttonId));
 
-        newButtonSet = ((currentSet.indexOf("urlbar-container") !== -1) ?
-            // Place the button before the urlbar
-            tb.currentSet.replace(/urlbar-container/, buttonId +",urlbar-container") :
-            // Place the button at the end of the urlbar
-            (tb.currentSet + buttonId));
+  tb.setAttribute("currentset", newButtonSet);
+  tb.currentSet = newButtonSet;
 
-        tb.setAttribute("currentset", newButtonSet);
-        tb.currentSet = newButtonSet;
-
-        chromeWin.document.persist("nav-bar", "currentset");
-        // If you don't do the following call, funny things happen
-        try {
-          chromeWin.BrowserToolboxCustomizeDone(true);
-        }
-        catch (e) { }
-        annodb.set("toolbar-button-installed", 1, utils.MOZID, function(){});
-
-        addToolbarButtonListener();
-        return;
-      });
+  chromeWin.document.persist("nav-bar", "currentset");
+  // If you don't do the following call, funny things happen
+  try {
+    chromeWin.BrowserToolboxCustomizeDone(true);
+  }
+  catch (e) { }
+  annodb.set("toolbar-button-installed", 1, utils.MOZID);
+  addToolbarButtonListener();
 }
 
 if (require.main === module.id) {
